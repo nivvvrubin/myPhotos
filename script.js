8683b552ac20064fbbb391cfe1d363e58cbda30f
nivvvrubin <niv.rbn@gmail.com>
2025-05-18 16:36:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // List of image URLs (host your images or use valid links)
 const images = [
-  'https://github.com/nivvvrubin/myPhotos/blob/main/a2.jpg', // replace with your actual image paths
+  'a1.jpg', // replace with your actual image paths//
   'a3.jpg',
   'myPhotos/a4.jpg'
   'a5.jpg'
