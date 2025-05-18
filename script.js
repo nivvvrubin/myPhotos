aee7178372c05946440abdf49060247fee2faf57
nivvvrubin <niv.rbn@gmail.com>
2025-05-18 16:17:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // List of image URLs (host your images or use valid links)
 const images = [
-  'myPhotos/a2.jpg', // replace with your actual image paths
+ /* 'myPhotos/a2.jpg', // replace with your actual image paths
   'myPhotos/a3.jpg',
   'myPhotos/a4.jpg'
-  'myPhotos/a5.jpg'
+  'myPhotos/a5.jpg'*/
 ];
 
 let currentIndex = 0;
